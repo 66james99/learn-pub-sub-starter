c58f05439ba18f739cb55d4469aedd33ad275229
66james99 <j.e.hammick@gmail.com>
2026-02-04 14:41:05 +0000

    my solution to CH3 LI before moving to the provided solution for future lessons

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,7 @@ import (
 	"os/signal"
 
 	"github.com/66james99/learn-pub-sub-starter/internal/pubsub"
+	"github.com/66james99/learn-pub-sub-starter/internal/routing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -26,6 +27,15 @@ func main() {
 		log.Fatalf("could not open RabbitMQ channel: %v", err)
 	}
 	defer channel.Close()
+
+	var message routing.PlayingState
+	message.IsPaused = true
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, message)
+	if err != nil {
+		log.Fatalf("could not publish message: %v", err)
+	}
+	fmt.Println("Message published successfully!")
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
